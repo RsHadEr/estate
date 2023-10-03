9931f54e9b278418817a6be185e10c82a85b9b8a
Rohan <rohanbrs123@gmail.com>
2023-10-03 10:39:48 +0530

    created the sign-in api route

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import Jwt  from "jsonwebtoken";
+import * as dotenv from 'dotenv';
+dotenv.config();
 const authRouter=express.Router();
 
-
+//api for sign up
 authRouter.post("/signup",async (req,res,next)=>{
 const {username,email,password}=req.body;
 const hashedPassword=bcryptjs.hashSync(password,10);
@@ -15,6 +19,27 @@ res.status(201).json({message:"usercreated"});
 {
     next(err);
 }
+});
+
+//api for sign in
+authRouter.post("/signin",async (req,res,next)=>{
+const {email,password}=req.body;
+try{
+const validUser=await User.findOne({email});
+if(!validUser)return next(errorHandler(404,"Invali Crediantials"));
+const validPassword=bcryptjs.compareSync(password,validUser.password);
+if(!validPassword)return next(errorHandler(401,"Invalid Credentials"));
+const token=Jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+const {password:pass,...rest}=validUser._doc;
+res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+
+
+
+}catch(err)
+{
+    next(err);
+}
+
 });
 
 
