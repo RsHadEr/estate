84bee06b7df4694073b746bcf65be054f834f5fb
Rohan <rohanbrs123@gmail.com>
2023-11-08 12:40:23 +0530

    Completed the Home page and About Page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -33,7 +33,7 @@ export default function Contact({listing}) {
             <div className="flex flex-col gap-2">
                 <p>Contact<span className='font-semibold'>{landLord.username}</span> for <span>{listing.name.toLowerCase()}</span></p>
                 <textarea name="message" id="message" cols="30" rows='30' value={message} onChange={onChange} placeholder='Enter your message' className='w-full border p-3 rounded-lg '></textarea>
-                <Link to={`mailto:${landLord.email}?subject=Regarding ${lisitng.name}&body=${message}`} className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity'>Send Message</Link>
+                <Link to={`mailto:${landLord.email}?subject=Regarding ${listing.name}&body=${message}`} className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity'>Send Message</Link>
             </div>
         )}
       
